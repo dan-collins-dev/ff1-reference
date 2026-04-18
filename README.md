0596a0e0d1a60ec02e9d00fc439b3d81f259d314
Dan Collins <dan.collins.dev@gmail.com>
2026-04-18 12:03:53 -0400

    stubbed out project's backend

---
+++ README.md
@@ -0,0 +1,7 @@
+# FF1 Reference
+
+## Project Overview
+
+This is a learning based project that I have attempted several times, but let drop to the wayside. I have a little more time that I can use to get this project off the ground.
+
+I'll be gathering data by playing the game with a 4 Monk party.
--- server.js
+++ server.js
@@ -5,9 +5,33 @@ import express from "express";
 const app = express();
 const port = 8080;
 
-app.get("/", () => {
+app.get("/api/items", () => {
     res.status(200).json({
-        message: "Initial Commit",
+        message: "TODO: Implement items route",
+    });
+});
+
+app.get("/api/spells", () => {
+    res.status(200).json({
+        message: "TODO: Implement spells route",
+    });
+});
+
+app.get("/api/chests", () => {
+    res.status(200).json({
+        message: "TODO: Implement chests route",
+    });
+});
+
+app.get("/api/monsters", () => {
+    res.status(200).json({
+        message: "TODO: Implement monsters route",
+    });
+});
+
+app.get("/api/locations", () => {
+    res.status(200).json({
+        message: "TODO: Implement locations route",
     });
 });
 
